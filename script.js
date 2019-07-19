d10bc3f37a19668cd7904690e153cc15b7a85adc
Luke Mansoor <lmansoor@ecc-jaguar.csuchico.edu>
2019-07-19 10:48:05 -0700

    Project Page

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function addnum(){
+function calculate()
+{
         var x = parseFloat(document.getElementById('numberin1').value);
         var y = parseFloat(document.getElementById('numberin2').value);
-        window.alert("The answer for " + x + "+" + y +" is: " + (x+y));
+        var operator = document.getElementById('operator').value;
+
+        window.alert("The answer is: " + operate(value1, value2, operator));
 }
 
+
+function operate(value1, value2, operator) {
+        if (operator == 'addition') {
+                return value1 + value2;
+        } else if (operator == 'subtraction') {
+                return value1 - value2;
+        } else if (operator == 'division') {
+                return value1 / value2;
+        } else if (operator == 'multiplication') {
+                return value1 * value2;
+        }
+}
