9ad5e44d3ba974387eec6595a3abd6c8367d7ae4
Luke Mansoor <lmansoor@ecc-jaguar.csuchico.edu>
2019-07-19 09:57:54 -0700

    Project Page

---
+++ script.js
@@ -0,0 +1,6 @@
+function addnum(){
+        var x = parseFloat(document.getElementById('numberin1').value);
+        var y = parseFloat(document.getElementById('numberin2').value);
+        window.alert("The answer for " + x + "+" + y +" is: " + (x+y));
+}
+
